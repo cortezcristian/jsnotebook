ff5edb5f124a3b064c75f71456d308a7911c12e0
Cristian Cortez <cortez.cristian@gmail.com>
2017-03-17 20:16:53 -0300

    Smart Row Component

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -2,10 +2,6 @@
 
 angular
   .module('jsnotebook')
-	.config(function($mdIconProvider) {
-    $mdIconProvider
-      .defaultIconSet('img/icons/sets/core-icons.svg', 24);
-  })
   .filter('keyboardShortcut', function($window) {
     return function(str) {
       if (!str) return;
@@ -28,14 +24,46 @@ angular
   })
   .controller('MainCtrl', function($scope, $timeout, $location, $log,
     $q, toastr, $uibModal, $mdDialog){
-	  this.settings = {
+
+		$scope.doc = {
+		 "data": [
+				{
+				 "rowtype": "markdown",
+				 "metadata": {},
+				 "source": [
+					"# JS Notebook"
+				 ]
+				},
+				{
+				 "rowtype": "markdown",
+				 "metadata": {},
+				 "source": [
+					"Download it from [jsnotebook](https://github.com/cortezcristian/jsnotebook/)"
+				 ]
+				},
+				{
+				 "rowtype": "code",
+				 "execution_count": 1,
+				 "metadata": {
+					"collapsed": false
+				 },
+				 "outputs": [],
+				 "source": [
+					"var os = require('os')\n",
+					"os.hostname()"
+				 ]
+				}
+			]
+		};
+
+	  $scope.settings = {
       printLayout: true,
       showRuler: true,
       showSpellingSuggestions: true,
       presentationMode: 'edit'
     };
 
-    this.sampleAction = function(name, ev) {
+    $scope.sampleAction = function(name, ev) {
       $mdDialog.show($mdDialog.alert()
         .title(name)
         .textContent('You triggered the "' + name + '" action')
@@ -43,6 +71,62 @@ angular
         .targetEvent(ev)
       );
     };
+
+	})
+	.directive('smartrow', function($log, $http, $compile, $parse, $templateCache){
+		 return {
+			 restrict: 'EA',
+			 template: "",
+			 scope: {
+         rowmodel : '=rowmodel'
+			 },
+			 link: function(scope, element, attrs){
+				// http://stackoverflow.com/questions/19501584/how-to-pass-in-templateurl-via-scope-variable-in-attribute
+
+				/*
+				scope.$watch(attrs.rowmodel, function (value) {
+					if (value) {
+						loadTemplate(value);
+					}
+				});
+				*/
+				 var templates = {
+					 'markdown': 'views/smartrow-markdown.html'
+				 };
+
+
+				if(scope.rowmodel && !scope.rowmodel.loaded){
+					loadTemplate(scope.rowmodel);
+				}
+
+				function loadTemplate(item) {
+					var template =  templates[item.rowtype] || "";
+					if(template !== "") {
+
+					$http.get(template, { cache: $templateCache })
+						.then(function(templateContent) {
+							console.log(template, templateContent);
+							debugger;
+							scope.rowmodel.loaded = true;
+							element.replaceWith($compile(templateContent.data)(scope));
+							//var s = angular.copy(scope);
+							//element.html($compile(templateContent.data)({}));
+						});
+					}
+				}
+
+
+			 }
+			 /*
+			 templateUrl: function(elem, attr){
+				 var templates = {
+					 'markdown': 'markdown'
+				 };
+				 $log.log(attr);
+				 return 'smartrow-markdown.html';
+			 }
+			 */
+		 };
 
    });
 
